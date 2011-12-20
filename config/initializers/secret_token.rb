c5080bc505feda321ecc4bebf8433ff14e6f543e
Ubuntu <ubuntu@domU-12-31-38-04-1C-3A.compute-1.internal>
2011-12-20 20:35:42 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hierarchical::Application.config.secret_token = '6dc31c2b185b350dbf80f2c80540ed08dd0508295d6eb3d478d260208a11747cb46deeae51724538be4245fd59995d72e62c3ff254434f43d269e611e8d3d587'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hierarchical::Application.config.session_store :cookie_store, key: '_hierarchical_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hierarchical::Application.config.session_store :active_record_store
